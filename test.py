ba62963bdebc99e4caa576c7ddbb91b5fbba19ac
dray89 <46060603+dray89@users.noreply.github.com>
2019-02-06 19:43:33 -0500

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import os, docx, keyword, untangle
-from xml.sax import make_parser, handler
-obj = untangle.parse("Program Evaluation_Memo.xml")
-doc = docx.Document()
-para = doc.add_paragraph(str(obj))
-doc.save("doc")
