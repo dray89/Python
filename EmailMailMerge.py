21b71a8425c2b1e228c7a4ab94db1010942fb5eb
dray89 <46060603+dray89@users.noreply.github.com>
2019-02-27 21:47:30 -0500

    Update EmailMailMerge.py

--- EmailMailMerge.py
+++ EmailMailMerge.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 from mailmerge import MailMerge
 from datetime import date
 
-   # Open the spreadsheet and get the latest dues status.
 template = input("Input Document Name and Extension: " )
 username = input("Input Sender Email: ")
 password = input("input password: ")
@@ -13,14 +12,10 @@ sheet = wb.get_sheet_by_name(input("Please enter Sheet Name: "))
 FinalDispositionColumn = sheet.get_highest_column()
 FinalDispositionStatus = sheet.cell(row=1, column=lastCol).value
 
-   # TODO: Check each contact's best disposition. Check to ensure all Final Dispositions are correct.
-
-   # TODO: Log in to email account.
-
-   # TODO: Send out reminder emails.
+   
 okay = ["Email Sent", "voicemail left"] #All FinalDispositions Okay to Contact
 contacts = {}
-for r in range(2, sheet.get_highest_row() + 1):
+for r in range(2, sheet.get_highest_row() + 1):  # Check each contact's best disposition. Check to ensure all Final Dispositions are correct.
     FinalDisposition = sheet.cell(row=r, column=lastCol).value
        if FinalDisposition in okay:
            name = sheet.cell(row=r, column=1).value
@@ -67,7 +62,7 @@ Subject: %s
 
 %s
 
-""" %(sent_from, to, subject, body)
+""" %(sent_from, to, subject, body)  # Log in to email account. Send out emails.
 server = smtplib.SMTP_SSL('smtp.domain', 465)
 server.ehlo_or_helo_if_needed()
 server.login(domain_user, domain_password)
