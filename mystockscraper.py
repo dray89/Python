ee18482a018b886d648c22d675422bf673eba038
dray89 <46060603+dray89@users.noreply.github.com>
2019-04-11 07:36:52 -0400

    Added notes
    Added documentation comments in notes

--- mystockscraper.py
+++ mystockscraper.py
@@ -4,7 +4,11 @@
 
 #Getting Started Guide: https://developer.tdameritrade.com/content/getting-started
 
-#Authorization token request and renewal: https://developer.tdameritrade.com/content/simple-auth-local-apps
+#Get User Principals: https://developer.tdameritrade.com/user-principal/apis/get/userprincipals-0
+
+#Simple App Authorization: https://developer.tdameritrade.com/content/simple-auth-local-apps
+
+#Post Access Token: https://developer.tdameritrade.com/authentication/apis/post/token-0
 
 #Request authorization from tdameritrade server
 auth_url = "https://api.tdameritrade.com/v1/oauth2/token"
@@ -15,7 +19,8 @@ body = {
 
 results = requests.get(auth_url, date=body)
 
-#Code is requested and comes out as an output. Redirect_url is a self-created url for the ameritrade server to communicate back results. Essentially, you can choose anything you want. Client_id is a self created alphanumeric string created to identify your application for authorization.
+#Code is requested and comes out as an output. Redirect_url is a self-created url for the ameritrade server to communicate back results. 
+#Essentially, you can choose anything you want. Client_id is a self created alphanumeric string created to identify your application for authorization.
 
 params = {
   'access_type': 'offline', 
