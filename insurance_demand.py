bf5fe0e02aedfa480f5318e972d4e8b93e3be704
dray89 <46060603+dray89@users.noreply.github.com>
2019-07-01 14:47:29 -0400

    Update insurance_demand.py

--- insurance_demand.py
+++ insurance_demand.py
@@ -1,5 +1,9 @@
-insured = calc_quantity(0, 70)
-coinsurance = calc_quantity(.5*list_price)
+list_price = 70
+uninsured = uninsured(list_price)
+insured = full(list_price)
+coinsurance = fifty_percent(list_price)
+
+coinsurance = calc_quantity(list_price)
 copay = calc_quantity(25)
 
 insured.quantity
@@ -13,7 +17,26 @@ class insurance_plans:
 		self_pay = self.self_pay
 		self.list_price = list_price
 		list_price = self.list_price
-
+	
+	@property
+	def get_self_pay(self)
+		return self.self_pay
+	
+	@get_self_pay.setter
+	def set_self_pay(self, self_pay):
+		self.self_pay = self_pay
+		amount = self.self_pay
+	
+	@property
+	def get_list_price(self):
+		return list_price
+	
+	@get_list_price.setter
+	def set_list_price(self, list_price):
+		self.list_price = list_price
+		list_price = self.list_price
+	
+		
 class uninsured(insurance_plans):
 	def uninsured_quantity(self):
 		uninsured_quantity = calc_quantity(self_pay = self.list_price)
@@ -22,9 +45,10 @@ class uninsured(insurance_plans):
 	def uninsured_price(self):
 		uninsured_price = calc_price(self_pay = self.list_price)
 		self.uninsured_price = uninsured_price
-		
-	def calc_quantity(self, self_pay):
-		self.self_pay = self_pay
+
+class full(insurance_plans):
+	def calc_quantity(self):
+		self.self_pay = 0
 		self.quantity = 100 - self_pay
 		self_quantity = self.quantity
 		
@@ -36,5 +60,21 @@ class uninsured(insurance_plans):
 		self.chg_qt = lambda chg_qt: quantity - self.uninsured_quantity 
 		self.social_loss = lambda social_loss: (chg_price*chg_qt)/2
 
+class fifty_percent(insurance_plans):
+	def calc_quantity(self):
+		self.self_pay = .5*list_price
+		self_pay = self.self_pay
+		
+		self.quantity = 100 - self_pay
+		self_quantity = self.quantity
+		
+	def calc_price(self, self_pay):
+		self.price = self_pay
+		
+	def social_loss(self_pay, self_quantity):
+		self.chg_price = lambda chg_price:  uninsured_price - self_pay  
+		self.chg_qt = lambda chg_qt: quantity - self.uninsured_quantity 
+		self.social_loss = lambda social_loss: (chg_price*chg_qt)/2
+end
 
 uninsured = dict(insurance_type = "uninsured", quantity = calc_quantity(list_price)),self_pay = list_price, list_price = list_price, price = list_price)
