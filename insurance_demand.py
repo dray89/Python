8bde816f57576dc17ce36529854c0c150f5cee90
dray89 <46060603+dray89@users.noreply.github.com>
2019-07-02 15:41:19 -0400

    Update insurance_demand.py

--- insurance_demand.py
+++ insurance_demand.py
@@ -10,20 +10,20 @@ class uninsured(insurance_plans):
 		insurance_plans.__init__(self, list_price)
 		self.uninsured_price = uninsured_price
 
-	def quantity(self):
-		self.uninsured_quantity = 100 - self.uninsured_price*self.list_price
+	def quantity(self, intercept = 100):
+		self.uninsured_quantity = intercept - self.uninsured_price*self.list_price
 
 class full(uninsured):
 	def __init__(self, list_price, self_pay = 0):
 		uninsured.__init__(self, list_price)
 		self.self_pay = self_pay
 	
-	def quantity(self):
-		self.quantity = 100 - self.self_pay*self.list_price
+	def quantity(self, intercept = 100):
+		self.quantity = intercept - self.self_pay*self.list_price
 	
-	def social_loss(self, uninsured_price = 1):
+	def social_loss(self, uninsured_price = 1, intercept = 100):
 		self.uninsured_pay = uninsured_price*self.list_price
-		self.uninsured_quantity = 100 - self.uninsured_pay
+		self.uninsured_quantity = intercept - self.uninsured_pay
 		self.social_loss = (self.uninsured_pay - self.self_pay*self.list_price)*(self.quantity - self.uninsured_quantity)/2
 
 class coinsure(uninsured):
@@ -31,13 +31,13 @@ class coinsure(uninsured):
 		uninsured.__init__(self, list_price)
 		self.self_pay = self_pay
 	
-	def quantity(self):
-		self.quantity = 100 - self.self_pay*self.list_price
+	def quantity(self, intercept = 100):
+		self.quantity = intercept - self.self_pay*self.list_price
 	
-	def social_loss(self, uninsured_price = 1):
+	def social_loss(self, uninsured_price = 1, intercept = 100):
 		self.uninsured_pay = uninsured_price*self.list_price
-		self.uninsured_p = 100 - self.quantity
-		self.uninsured_quantity = 100 - self.uninsured_pay
+		self.uninsured_p = intercept - self.quantity
+		self.uninsured_quantity = intercept - self.uninsured_pay
 		self.social_loss = (self.list_price - self.uninsured_p)*(self.quantity - self.uninsured_quantity)/2
 
 class copay(uninsured):
@@ -45,12 +45,12 @@ class copay(uninsured):
 		uninsured.__init__(self, list_price)
 		self.self_pay = self_pay
 	
-	def quantity(self):
-		self.quantity = 100 - self.self_pay
+	def quantity(self, intercept = 100):
+		self.quantity = intercept - self.self_pay
 	
-	def social_loss(self, uninsured_price = 1):
+	def social_loss(self, uninsured_price = 1, intercept = 100):
 		self.uninsured_pay = uninsured_price*self.list_price
-		self.uninsured_quantity = 100 - self.uninsured_pay
+		self.uninsured_quantity = intercept - self.uninsured_pay
 		self.social_loss = (self.uninsured_pay - self.self_pay)*(self.quantity - self.uninsured_quantity)/2
 
 end
