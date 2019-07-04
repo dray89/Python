640b91eaaa116f571fa5a0c72f6bb03f9abf8a91
dray89 <46060603+dray89@users.noreply.github.com>
2019-07-03 21:11:26 -0400

    Update insurance_demand_v2.py

--- insurance_demand_v2.py
+++ insurance_demand_v2.py
@@ -5,89 +5,104 @@ This is a program to calculate attributes belonging to insurance plans.
 
 class InsurancePlans:
     """
-    This is a class for insurance plans.    
-    Attributes: 
+    This is a class for insurance plans.
+    Attributes:
         list price (float): The price at which the service is offered.
         intercept (float): The intercept of the demand function.
     """
-    def __init__(self, list_price, intercept=100, perc_coverage=1.00):
+    def __init__(self, list_price, intercept=100.00, perc_coverage=0.00, copay = 0.00, uninsured_perc = 100):
         self.list_price = list_price
         self.intercept = intercept
-        self.perc_coverage = perc_coverage
+        self.uninsured_price = self.uninsured_perc*self.list_price
 
-    def uninsured(self, list_price, uninsured_perc=1.00):
-        """ 
-        This is a method to define and calculate the base state - uninsured.
-        Attributes:
-            uninsured_price (float)
-            uninsured_quantity (int)
-            uninsured_perc (float)
-            """
-        uninsured_price = uninsured_perc*list_price
-        uninsured_quantity = self.intercept - self.uninsured_price*self.list_price
-        
-    def demand_function(x_value, intercept=100, slope=1):
-        intercept = intercept
-        slope = slope
-        x_value = x_value
-        y_value = intercept + slope*x_value
-        return y_value
+    def uninsured_qty(self):
+        """
+        This is a method to define and calculate the base quantity in the uninsured state.
+        """
+        return self.intercept - self.uninsured_price()
+
+    def uninsured_price(self):
+        """
+        This is a method to define and calculate the base price in the uninsured state.
+        """
+        return self.uninsured_perc*self.list_price
+
+    def fair_partial(self):
+        pass
+    
+    def unfair_partial(self):
+        pass
+    
+    def benefit(self):
+        pass
+    
+    def premium(self):
+        pass
+    
     
 class Full(InsurancePlans):
-    """ 
-    This is a class for full insurance plans.
-    Attributes:
-            price (float)
-            intercept (float)
-            uninsured_perc (float)
     """
-    
-    def __init__(self, list_price, intercept=100, self_pay=0.00):
-        InsurancePlans.__init__(self, list_price, intercept)
-        self.self_pay = self_pay
+    This is a class for full insurance plans.
+    """
+
+    def __init__(self, list_price, uninsured_perc, uninsured_price, intercept=100):
+        InsurancePlans.__init__(self, list_price, uninsured_perc, self.uninsured_price(), intercept)
 
     def quantity(self):
-        quantity = self.intercept - self.self_pay*self.list_price
-        return quantity
-    
+        """
+        This method calculates the quantity demanded of full insurance plans
+        """
+        return self.intercept - self.self_pay*self.list_price
+
     def social_loss(self):
-        social_loss = .5*self.list_price**2*(self.uninsured_price - self.self_pay)**2
-        return social_loss
+        """
+        This method calculates the social loss from the uninsured state.
+        """
+        return .5*self.list_price**2*(self.uninsured_price - self.self_pay)**2
+
+    def unfair_premium(self):
+        pass
+    
+    def fair_premium(self):
+        pass
     
 class Coinsure(InsurancePlans):
-    """ 
-    This is a class for coinsurance plans.
-    Attributes:
-            price (float)
-            intercept (float)
-            uninsured_perc (float)
     """
-    
-    def __init__(self, list_price, intercept=100, self_pay=.5):
+    This is a class for coinsurance plans.
+    """
+    def __init__(self, list_price, intercept=100, perc_cover=.5):
         InsurancePlans.__init__(self, list_price, intercept)
-        self.self_pay = self_pay
+        self.perc_cover = perc_cover
+        
+    def quantity(self):
+        """
+        This method calculates the quantity demanded of coinsurance insurance policies.
+        """
+        return self.intercept - self.self_pay*self.list_price
 
-    def calc_quantity(self):
-        quantity = self.intercept - self.self_pay*self.list_price
-        return quantity
-    
     def social_loss(self):
-        social_loss = .5*(self.list_price*(1-self.self_pay))**2
-        return social_loss
+        """
+        This method calculates the social loss from the uninsured state.
+        """
+        return .5*(self.list_price*(1-self.self_pay))**2
     
 class Copay(InsurancePlans):
-    """ 
-    This is a class for copay plans.    
     """
-
-    def __init__(self, list_price, intercept=100, self_pay=25):
+    This is a class for copay plans.
+    """
+    def __init__(self, list_price, intercept=100, copay=25):
         InsurancePlans.__init__(self, list_price, intercept)
-        self.self_pay = self_pay
+        self.copay = copay
 
     def quantity(self):
-        quantity = self.intercept - self.self_pay
-        return quantity
-    
+        """
+        This method calculates the quantity demanded of copay insurance policies.
+        """
+        return self.intercept - self.copay
+
     def social_loss(self):
-        social_loss = .5*(self.list_price-self.self_pay)**2
-        return social_loss
+        """
+        This method calculates the social loss from the uninsured state.
+        """
+        return .5*(self.list_price-self.copay)**2
+    
