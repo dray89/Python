f98171d7f93c09d0ae8f5ddbe92d2477d98ac6c3
Debra Ray <raydebra89@gmail.com>
2019-10-29 08:07:59 -0400

    update

--- update_pypi.py
+++ update_pypi.py
@@ -4,7 +4,7 @@ Created on Tue Oct  1 14:10:49 2019
 
 @author: rayde
 """
-
+cd C:\Users\rayde\iCloudDrive\GitHub\finance_python
 python setup.py sdist
 twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 
