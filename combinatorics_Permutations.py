3efca15762bed612653dd6bfcfd1f607f4120d96
Debra Ray <raydebra89@gmail.com>
2020-10-15 21:07:37 -0500

    moved files
    combined combinations with permutations, moved stata_python into main folder

--- combinatorics_Permutations.py
+++ combinatorics_Permutations.py
@@ -4,14 +4,28 @@ Created on Sat Oct 10 22:35:57 2020
 
 @author: rayde
 """
+
+'''
+Get all the possible permutations for a set of objects.
+'''
+
+from itertools import product
+import pandas as pd
+    
+def perm_possible(items, repeat=5):
+    perm = product(items, repeat)
+    return [i for i in list(perm)]
+
+perm = perm_possible([0,1])
+
+
+
 '''
 Two ways to manually code combinatorics in python without replacement.
 
-This isn't from leetcode, but an exercise in combinatorics from math for data science
-specialization on Coursera. 
 '''
 
-
+#Method 1
 n=8
 count = 0
 
@@ -23,9 +37,12 @@ for i in range(n):
 
 print(count)
  
+#Method 2
 #C(n,r)=n!/(n−r)!r!
 
 def combinatorics(n, k):
+    
+    # Calculate n-factorial  
     n_list = list(range(2, n))
     n_list.reverse()
     n_factorial = n
@@ -33,7 +50,7 @@ def combinatorics(n, k):
     for each in n_list:
         n_factorial = n_factorial*(each)
 
-    
+    # Calculate n minus k factorial 
     n_r  = n-k
     
     n_r_list = list(range(2, n_r))
@@ -44,6 +61,7 @@ def combinatorics(n, k):
     for each in n_r_list:
         n_r_factorial = each*n_r_factorial
     
+    # Calculate k factorial
     k_list = list(range(2, k))
     k_list.reverse()
     k_factorial = k
@@ -51,4 +69,5 @@ def combinatorics(n, k):
     for each in k_list:
         k_factorial = k_factorial*(each)
     
+    # Plug into formula and return
     return n_factorial/(n_r_factorial*k_factorial)
