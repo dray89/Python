ad30f8049fa48d74beafa4975ce010f9b6c3ea4d
Debra Ray <raydebra89@gmail.com>
2020-11-02 19:17:24 -0500

    added leetcode exercises

---
+++ Leetcode/reduce_to_zero.py
@@ -0,0 +1,24 @@
+'''
+
+Given a non-negative integer num, return the number of steps to reduce it to zero. If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+ 
+'''
+
+import numpy as np 
+
+class Solution(object):
+    def numberOfSteps (self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        num = np.array(num)
+        steps = 0
+        while num>0:
+            if num % 2 == 0:
+                num = num/2
+            else:
+                num = num - 1
+            steps +=1
+        return steps
