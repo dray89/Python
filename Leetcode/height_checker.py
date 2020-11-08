be1c4fcf4140eaf43ecf34513bc6ec82523c7123
Debra Ray <raydebra89@gmail.com>
2020-11-08 18:48:30 -0500

    Update height_checker.py

--- Leetcode/height_checker.py
+++ Leetcode/height_checker.py
@@ -1,3 +1,11 @@
+'''
+Students are asked to stand in non-decreasing order of heights for an annual photo.
+
+Return the minimum number of students that must move in order for all students to be standing in non-decreasing order of height.
+
+Notice that when a group of students is selected they can reorder in any possible way between themselves and the non selected students remain on their seats.
+
+'''
 class Solution(object):
     def heightChecker(self, heights):
         """
@@ -5,8 +13,7 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        for each in list(zip(sorted(heights), heights)):
-            if each[0] != each[1]:
-                count+=1
+        for a, b in list(zip(sorted(heights), heights)):
+            if a != b:
+                count += 1
         return count
-        
